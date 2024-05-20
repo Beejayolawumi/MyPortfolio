0710bda68388b0b2558f2e47d71441e26cfea8e3
unknown <beejayolawumi2020@gmail.com>
2024-05-20 11:43:41 +0100

    updated

--- src/components/Aboutpage/Aboutpage.jsx
+++ src/components/Aboutpage/Aboutpage.jsx
@@ -24,7 +24,7 @@ const Aboutpage = () => {
         <div className="hero-connect">Connect with me</div>
         <div className="hero-resume">
           <a
-            href="https://drive.google.com/file/d/1c27lQLv6pn9pv1JKA1riWjI4xCNv3m9o/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/1LlIYqERA3e_FTrmFWMTBqKk3W9filmDU/view?usp=drive_link"
             target="blank"
             className="resume"
           >
